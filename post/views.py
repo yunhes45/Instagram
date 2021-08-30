ca7dc73fa46d95220f03124a2abe679b50b2f0ac
yunhes45 <yunhes45@gmail.com>
2021-08-30 22:06:42 +0900

    [#17] Back : board post

--- post/views.py
+++ post/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import get_user_model
 from .models import Post
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -20,3 +23,56 @@ def post_list(request):
         return render(request, 'post/post_list.html', {
             'posts': post_list
         })
+
+@login_required
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            # post.tag_save()
+            messages.info(request, '새 글이 등록되었습니다.')
+            return redirect('post:post_list')
+    else:
+        form = PostForm()
+    return render(request, 'post/post_new.html', {
+        'form': form,
+    })
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author != request.user:
+        messages.warning(request, '잘못된 접근입니다.')
+        return redirect('post:post_list')
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            # post.tag_set.clear()
+            # post.tag_save()
+            messages.success(request, '수정완료')
+            return redirect('post:post_list')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'post/post_edit.html', {
+        'post': post,
+        'form': form,
+
+    })
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if post.author != request.user or request.method == 'GET':
+        messages.warning(request, '잘못된 접근입니다.')
+        return redirect('post:post_list')
+
+    if request.method == 'POST':
+        post.delete()
+        messages.success(request, '삭제완료')
+        return redirect('post:post_list')
